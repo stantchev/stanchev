f43e97fd1eb2a5a86e937b8495117d71c6481562
Stanchev <stantcheff@gmail.com>
2025-05-01 11:49:22 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import AboutSection from './components/AboutSection';
@@ -8,17 +9,22 @@ import ContactSection from './components/ContactSection';
 import Footer from './components/Footer';
 
 function App() {
+const { i18n } = useTranslation();
+
   useEffect(() => {
     // Update page title
-    document.title = 'STANCHEV | Portfolio';
+    document.title = 'Stanchev | Portfolio';
     
     // Add a class to the body for global styling
     document.body.classList.add('bg-[#050816]', 'text-white');
+	
+	// Update lang attribute based on current language
+    document.body.setAttribute('lang', i18n.language);
     
     return () => {
       document.body.classList.remove('bg-[#050816]', 'text-white');
     };
-  }, []);
+  }, [i18n.language]);
 
   return (
     <div className="relative z-0">
