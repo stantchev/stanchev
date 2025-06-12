d714775063613f5fbeed691c3e895329150e1a16
Stanchev <stantcheff@gmail.com>
2025-06-12 11:09:30 +0300

    Update Layout.tsx

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -7,11 +7,6 @@ import { Helmet } from 'react-helmet-async';
 const Layout: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col">
-      <Helmet>
-        <html lang="bg" />
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Helmet>
       <Navbar />
       <main className="flex-grow">
         <Outlet />
