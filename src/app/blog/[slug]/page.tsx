64de5e667c5a26cce413701897d6104d6f5933e1
Stanchev <stantcheff@gmail.com>
2025-07-07 12:24:23 +0300

    Update page.tsx

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -34,7 +34,7 @@ export async function generateMetadata({
     },
     image: post.metadata.image || `/api/og/generate?title=${post.metadata.title}`,
     path: `${blog.path}/${post.slug}`,
-  });
+  };
 }
 
 export default async function Blog({
