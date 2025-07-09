a8255617198d17d47a9a1d5dbf6c33fa65cf7986
Stanchev <stantcheff@gmail.com>
2025-07-09 13:42:45 +0300

    Update page.tsx

--- src/app/seo-uslugi/keyword-research/page.tsx
+++ src/app/seo-uslugi/keyword-research/page.tsx
@@ -41,7 +41,7 @@ export async function generateMetadata() {
       siteName: seoServices.title,
       images: [
         {
-          url: `https://stanchev.vercel.app/images/og/og.jpg`,
+          url: `https://stanchev.bg/images/og/og.jpg`,
           width: 1200,
           height: 630,
         },
@@ -53,7 +53,7 @@ export async function generateMetadata() {
       card: 'summary_large_image',
       title: 'Keyword Research – основата на успешната SEO стратегия',
       description: 'Откриваме най-ценните ключови думи за вашия бизнес с професионален keyword research и анализ.',
-      images: [`https://stanchev.vercel.app/images/og/og.jpg`],
+      images: [`https://stanchev.bg/images/og/og.jpg`],
     },
   };
 }
