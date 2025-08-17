0fcb5a5124be7f6ad78e485a37fc1fe28d7140cd
Stanchev <stantcheff@gmail.com>
2025-08-17 18:39:59 +0300

    Create data.config.js

---
+++ src/app/recources/data.config.js
@@ -0,0 +1,11 @@
+variant: "gradient", // flat | gradient | outline
+mode: "categorical", // categorical | divergent | sequential
+height: 24, // default chart height
+axisLine: {
+  stroke: "var(--neutral-alpha-weak)",
+},
+tick: {
+  fill: "var(--neutral-on-background-weak)",
+  fontSize: 11,
+},
+tickLine: false,
