9e5113ddb12e59a8f1254fd8cac02bb4ce87430a
Stanchev <stantcheff@gmail.com>
2025-09-30 10:54:58 +0300

    Update config.types.ts

--- src/types/config.types.ts
+++ src/types/config.types.ts
@@ -142,7 +142,9 @@ export type SchemaConfig = {
  * Social links for organization.
  */
 export type SameAsConfig = {
-[key: string]: string;
+  linkedin?: string;
+  github?: string;
+  devto?: string;
 };
 
 /**
