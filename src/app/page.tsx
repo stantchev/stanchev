ab6ff8611f93e20cdad07e73cd58c7480b1298d4
Stanchev Digital <stantcheff@gmail.com>
2026-04-05 11:44:39 +0300

    Change greeting from 'Hello' to 'Goodbye'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -310,7 +310,7 @@ export default function Nachalo() {
           paddingTop="xl"
         >
           <RevealFx speed="fast">
-            <Badge title="New release" icon="rocket" arrow={false} effect={false}/>
+            <Badge title="Услуги" icon="rocket" arrow={false} effect={false}/>
               SEO + React + Next.js · София, България
             </Badge>
           </RevealFx>
